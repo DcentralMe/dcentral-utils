1ddc72fd11308976fe554ac9c77211c25fd35e10
tenthirtyone <alexsherbuck@gmail.com>
2024-04-30 17:30:07 -0500

    bare minimum, no safety checks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Fucking Fuck");
+import { Seaport } from "@opensea/seaport-js";
+import { ethers } from "ethers";
+
+const provider = new ethers.BrowserProvider(window.ethereum);
+
+export const seaport = new Seaport(provider);
